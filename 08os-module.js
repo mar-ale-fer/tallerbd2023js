73725bea3abd0c6b15b3d5281c9a6d8a2b0e361e
Marcelo Fernández <mar.ale.fer@gmail.com>
2023-03-30 20:56:28 -0300

    added system uptime

--- 08os-module.js
+++ 08os-module.js
@@ -2,3 +2,6 @@ const os =require ("os")
 //info about current user
 const user = os.userInfo()
 console.log(user)
+const uptime = os.uptime()
+console.log(`system uptime is ${uptime} seconds`)
+console.log("system uptime is "+uptime+" seconds")
