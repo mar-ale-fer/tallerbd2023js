4d5da48cd2de8047bbb0f410b4d14570505ed103
Marcelo Fernández <mar.ale.fer@gmail.com>
2023-03-30 20:43:35 -0300

    first node app

---
+++ 01intro.js
@@ -0,0 +1,20 @@
+console.log("hola mundo")
+const amount =12
+if (amount<10) {
+    console.log("small number")
+}else{
+    console.log ("large number")
+
+} 
+
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env)
+
+setInterval(() => {
+    console.log("taller base de datos")
+},1000)
+
+setInterval(() => {
+    console.log("mas lento -----")
+},2000)
