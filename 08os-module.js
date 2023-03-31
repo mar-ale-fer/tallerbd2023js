537eed3fdf8aeede82bdb82f43ef4fce1527239a
jesica jara <jesi.jara93@gmail.com>
2023-03-30 21:12:53 -0300

    agregado release

--- 08os-module.js
+++ 08os-module.js
@@ -5,3 +5,12 @@ console.log(user)
 const uptime = os.uptime()
 console.log(`system uptime is ${uptime} seconds`)
 console.log("system uptime is "+uptime+" seconds")
+
+const currentOS = {
+    name: os.type (),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+
+}
+console.log(currentOS)
