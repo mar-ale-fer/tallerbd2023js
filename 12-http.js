e381163e7f727d8f6eecbb25c5d65b1276c88ec9
Marcelo Alejandro Fernández <mar.ale.fer@gmail.com>
2023-05-04 21:27:01 -0300

    ajustes boton

--- 12-http.js
+++ 12-http.js
@@ -4,6 +4,10 @@ const path = require('path')
 const server = http.createServer((req, res)  => {
     var filePath = path.join(__dirname, 'index.html');
     var stat = fileSystem.statSync(filePath);
+
+    var filePath2 = path.join(__dirname, 'botonhw.jpg');
+    var stat2 = fileSystem.statSync(filePath);
+
     if (req.url=== '/') { 
         res.end('WELCOME TO OUR HOME PAGE')
     } else if (req.url === '/about') {
@@ -15,6 +19,14 @@ const server = http.createServer((req, res)  => {
             'Content-Length': stat.size
         });
         var readStream = fileSystem.createReadStream(filePath);
+        // We replaced all the event handlers with a simple call to readStream.pipe()
+        readStream.pipe(res)
+    } else if (req.url === '/boton') {
+        res.writeHead(200, {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': stat2.size
+        });
+        var readStream = fileSystem.createReadStream(filePath2);
         // We replaced all the event handlers with a simple call to readStream.pipe()
         readStream.pipe(res)
 
